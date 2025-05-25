0e5eb13371849faa527b6cdaa8376f3bbcb3b7ed
RossVMartin <104238164+RossVMartin@users.noreply.github.com>
2025-05-25 15:53:08 +0100

    added compression (dictionary json file is optimized heavily now)

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import compression from 'compression';
 import { handler } from './build/handler.js'; // The handler for SvelteKit
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+app.use(compression());
 
 app.use(handler);
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
 	console.log(`Running express server on http://localhost:${port}`);
 });
